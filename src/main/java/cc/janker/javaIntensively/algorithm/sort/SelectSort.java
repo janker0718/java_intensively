b5dc44e8f3d6497043d928084646e23a66ba1d90
janker0718 <liuzhiyong0718@sina.com>
2017-05-09 19:02:26 +0800

    选择排序

--- src/main/java/cc/janker/javaIntensively/algorithm/sort/SelectSort.java
+++ src/main/java/cc/janker/javaIntensively/algorithm/sort/SelectSort.java
@@ -2,12 +2,13 @@ package cc.janker.javaIntensively.algorithm.sort;
 
 /**
  * 选择排序
+ * 
  * @author janker0718(www.janker.cc)
  *
  */
 public class SelectSort {
 	public static void main(String[] args) {
-		int[] a ={1,233,23,21,34};
+		int[] a = { 1, 233, 23, 21, 34 };
 		sort(a);
 		for (int i : a) {
 			System.out.println(i);
@@ -17,16 +18,37 @@ public class SelectSort {
 	private static void sort(int[] a) {
 		for (int i = 0; i < a.length; i++) {
 			int min = a[i];
-			int n =i;	//最小数的索引
-			for (int j = i+1; j < a.length; j++) {
-				//从第二个开始找出最小数
+			int n = i;
+			for (int j = i + 1; j < a.length; j++) {
 				if (a[j] < min) {
 					min = a[j];
 					n = j;
 				}
+
 			}
 			a[n] = a[i];
 			a[i] = min;
 		}
 	}
+
+	private static void sort2(int[] a) {
+		for (int i = 0; i < a.length; i++) {
+			// 设置当前选中的值为最小值
+			int min = a[i];
+			int n = i; // 最小数的索引
+			for (int j = i + 1; j < a.length; j++) {
+				// 从第二个开始找出最小数
+				if (a[j] < min) {
+					// 设置最小值
+					min = a[j];
+					// 最小值的索引
+					n = j;
+				}
+			}
+			// a[i]与min交换位置
+			a[n] = a[i];
+			a[i] = min;
+		}
+	}
+
 }
